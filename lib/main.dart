6426dbfaad629ffc21d708339a32cc1f869c9d1c
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-11-04 14:58:02 +0530

    Splash Screen Implementation and Code Structure Implementation

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Modules/Screens/Splash_Screen/Views/splash_screen.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      getPages: [
+        GetPage(name: "/", page: () => SplashScreen()),
+      ],
+    ),
+  );
+}
